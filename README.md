5a9d8b380572b92a7e22a1ea9427c782d61a76e7
Daniel Kelley <danielrobertkelley@gmail.com>
2018-05-16 22:46:43 -0400

    hw4

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # unit-4-game
+This is a Crystal Gem game showing the use of Javascript and Jquery
+By using Jquery we are using a loaded items from a library 
+the object of the game is to create a random generated number and have the user click on the gems to reach the desired amount
+Each gem should also create a random generartor number 
+if the user can get to the targeted amount the user wins
+if the user goes over the targeted amount the user loses
+
--- assets/javascript/gems.js
+++ assets/javascript/gems.js
@@ -1,62 +1,118 @@
 $(document).ready(function () {
 
-    var targetNumber = Math.floor(Math.random())
+    var targetNumber = Math.floor(Math.random() * 101 + 19);
     var crystals = $("#crystals");
-    var imageCrystal = $("<img>");
-
-
+    var crystalYellow = $("#crystal-yellow");
+    //var crystalYellow = Math.floor(Math.random());
+    //var crystalRed = Math.floor(Math.random());
+    //var crystalBlue = Math.floor(Math.random());
+    //var crystalGreen = Math.floor(Math.random())
     $("#number-to-guess").text(targetNumber);
 
     var crystals = $("#crystals");
+    $("#crystals").attr("id", "crystal-yellow", "crystal-red", "crystal-blue", "crystal-green");
 
     var counter = 0;
+    counter = $("#counter");
 
-    var numberOptions = Math.floor(Math.random())
-    var cystalValue = numberOptions;
+    $("#crystals").attr("id", "crystal-yellow", "crystal-red", "crystal-blue", "crystal-green");
+    var numberOptions = Math.floor(Math.random() * 101 + 19);
 
-    for (i = 0; i < numberOptions.length; i++) {
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-yellow", "crystal-red", "crystal-blue", "crystal-green");
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        crystals.append(imageCrystal);
-    }
-    crystals.on("click", ".crystal-yellow", function () {
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
+    function intialize(targetNumber) {
+        for (var i = 0; i < targetNumber.length; i++) {
+            var targetNumber = $("#number-to-guess");
+            targetNumber.text(targetNumber[i]);
+            $("#number-to-guess").append(targetNumber);
+        }
+
+        for (var j = 0; j < 4; j++) {
+            var numberOptions = Math.floor(Math.random() * 101 + 19);
+            var numberOptions = $("#crystals");
+            numberOptions.text("#counter");
+            $("#counter").text(numberOptions);
+        };
+
         $("#counter").text(numberOptions);
+        $("#counter").append(numberOptions);
+    }
+
+    $("#crystal-yellow").on("click", function () {
+        counter = counter + $("#crystal-yellow");
+        $("#counter").text(numberOptions);
+        console.log(numberOptions);
+        if (counter == targetNumber){
+            win();
+          }
+          else if ( counter > targetNumber){
+            lose();
+          }
     })
 
-    crystals.on("click", ".crystal-red", function () {
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
+    $("#crystal-red").on("click", function () {
         $("#counter").text(numberOptions);
+        count = numberOptions + $("#crystal-red");
+        console.log(numberOptions);
+        if (counter == targetNumber){
+            win();
+          }
+          else if ( counter > targetNumber){
+            lose();
+          }
 
-        crystals.on("click", ".crystal-blue", function () {
-            var crystalValue = ($(this).attr("data-crystalvalue"));
-            crystalValue = parseInt(crystalValue);
-            counter += crystalValue;
-            $("#counter").text(numberOptions);
+    })
 
-            crystals.on("click", ".crystal-green", function () {
-                var crystalValue = ($(this).attr("data-crystalvalue"));
-                crystalValue = parseInt(crystalValue);
-                counter += crystalValue;
-                $("#counter").text(numberOptions);
-                console.log(numberOption);
-            });
-        });
+    $("#crystal-blue").on("click", function () {
+        $("#counter").text(numberOptions);
+        count = numberOptions + $("#crystal-blue");
+        console.log(numberOptions);
+        if (counter == targetNumber){
+            win();
+          }
+          else if ( counter > targetNumber){
+            lose();
+          }
+    })
+
+    $("#crystal-green").on("click", function () {
+        $("#counter").text(numberOptions);
+        count = numberOptions + $("#crystal-green");
+        console.log(numberOptions);
+        if (counter == targetNumber){
+            win();
+          }
+          else if ( counter > targetNumber){
+            lose();
+          }
+
     });
-})
-var wins = [];
-var loses = [];
-function checkWin () {
-if(counter == targetNumber)
-    alert("You Win");
-    $("#wins").text(wins)
-} {
-if (counter >= targetNumber)
-    alert("You Lose")
-    $("#loses").text(loses)
-}
+
+
+    var wins = [];
+    var loses = [];
+    function checkWin() {
+
+        $("#wins").text(wins);
+        $("#loses").text(loses);
+    }
+    function reset() {
+        targetNumber = Math.floor(Math.random() * 101 + 19);
+        $("#number-to-guess").text(targetNumber);
+        crystalYellow = Math.floor(Math.random() * 100 + 19);
+        crystalRed = Math.floor(Math.random() * 102 + 19);
+        crystalBlue = Math.floor(Math.random() * 103 + 19);
+        crystalGreen = Math.floor(Math.random() * 104 + 19);
+        counter = 0;
+        $("#total").text(counter);
+
+    }
+    function win() {
+        alert("you win");
+        wins++;
+        $("#wins").text(wins);
+    }
+    function lose() {
+        alert("you lose");
+        loses++;
+        $("#loses").text(loses);
+    }
+});
